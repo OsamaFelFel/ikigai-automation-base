76822b1408c83844a0fd522175d4fd5927d9f2a6
Osama Felfel <osama.felfel@hotmail.com>
2018-09-12 15:43:23 +0200

    Initial version

---
+++ src/test/java/comapny/ikigai/selenium/config/DriverSetup.java
@@ -0,0 +1,8 @@
+package comapny.ikigai.selenium.config;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public interface DriverSetup {
+    RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities);
+}
